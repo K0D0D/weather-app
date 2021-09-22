279c8538c1f4c1ff244e5956a744929647337c31
K0D0D <kovach.daniil.js@gmail.com>
2021-09-22 16:02:12 +0300

    added geoposition api and map update

---
+++ src/js/geolocation.js
@@ -0,0 +1,42 @@
+function getPosition () {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+}
+
+export async function positionHandler () {
+    let position = {};
+
+    try {
+        const response = await getPosition();
+
+        position.lat = response.coords.latitude;
+        position.lon = response.coords.longitude;
+
+        localStorage.setItem("position", JSON.stringify(position));
+    } catch (err) {
+        if (!localStorage.getItem("position")) {
+            position.lat = 51.507351;
+            position.lon = -0.127758;
+        } else {
+            position = JSON.parse(localStorage.getItem("position"));
+        }
+    }
+
+    console.log(position);
+    updateMap(position);
+}
+
+export function updateMap (locationObj) {
+    const mapIframe = document.querySelector("#map-iframe") || document.createElement("iframe");
+
+    if (!mapIframe.id) {
+        mapIframe.id = "map-iframe";
+        document.querySelector(".map").appendChild(mapIframe);
+    }
+
+    mapIframe.src = "https://maps.google.com/maps?q=" +
+                    locationObj.lat + "," + 
+                    locationObj.lon + 
+                    "&z=11&output=embed";
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,9 @@ import "../index.html";
 import "../scss/style.scss";
 import "focus-visible";
 
-console.log("Hello world")
+import * as geolocation from "./geolocation";
+
+document.addEventListener("DOMContentLoaded", () => {
+    geolocation.positionHandler();
+});
+
